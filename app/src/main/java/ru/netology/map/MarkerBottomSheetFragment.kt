2f17adc2c836d5a70a0e0054b44f7c0dbc3d3f4b
Ekaterina <rfvb.foto@mail.ru>
2021-11-05 19:08:33 +0300

    commit

---
+++ app/src/main/java/ru/netology/map/MarkerBottomSheetFragment.kt
@@ -0,0 +1,29 @@
+package ru.netology.map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_marker_info.*
+
+class MarkerBottomSheetFragment : BottomSheetDialogFragment() {
+    private var isDeleted = false
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return  inflater.inflate(R.layout.fragment_marker_info, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        marker_title.text = arguments?.getString("title")
+        marker_coordinates.text = arguments?.getString("coordinates")
+    }
+
+}
+
---
+++ app/src/test/java/ru/netology/map/MarkerBottomSheetFragment.kt
@@ -0,0 +1,5 @@
+package ru.netology.map
+
+class MarkerBottomSheetFragment {
+
+}
